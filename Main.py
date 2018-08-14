5a7266efaa6f533cb2c4930fc8c072a05f00c920
Rafael <rafael.vasel@gmail.com>
2018-08-13 21:09:54 -0300

    mudanças

--- Main.py
+++ Main.py
@@ -1,7 +1,14 @@
+import os
 import os.path
+import shutil
 
 import couch
-import hbase
+
+
+# grava o arquivo em outra pasta, pronto para ser importado no hbase
+def copiaArquivo(pastaAntiga, arquivoAntigo, pastaNova, arquivoNovo):
+    shutil.copy(os.path.join(pastaAntiga, arquivoAntigo), os.path.join(pastaNova, arquivoNovo))
+    # os.rename(os.path.join(pastaNova, arquivoAntigo), os.path.join(pastaNova, arquivoNovo))
 
 # Le os dicoms em C:\Users\Rafael\Documents\POS-DATA-SCIENCE\NoSql\DICOMdataset
 pasta = 'C:/Users/Rafael/Documents/POS-DATA-SCIENCE/NoSql/DICOMdataset/'
@@ -9,4 +16,5 @@ for dirpath, dirnames, filenames in os.walk(pasta):
     for filename in [f for f in filenames if f.endswith(".dcm")]:
         arquivo = os.path.join(dirpath, filename)
         # Insere os arquivos binarios no HBASE com o retorno da funcao no couch . Campo: SOPInstanceUID
-        hbase.insereHbase(couch.insereArquivo(arquivo), arquivo)
+        copiaArquivo(dirpath, filename, 'C:/Users/Rafael/Documents/POS-DATA-SCIENCE/NoSql/hbase-files/',
+                     couch.insereArquivo(arquivo))
--- couch.py
+++ couch.py
@@ -1,6 +1,7 @@
 import couchdb
 import pydicom
 
+
 user = "admin"
 password = "admin"
 couchserver = couchdb.Server("http://%s:%s@localhost:5984/" % (user, password))
@@ -15,14 +16,26 @@ def insereCouch(id, tags):
     db[id] = eval(tags)
 
 
+
 def insereArquivo(arquivo):
     dataset = pydicom.dcmread(arquivo, stop_before_pixels=False)  # .values()
     tags = ''
+    valor = ''
     for de in dataset:
-        if type(de.value) is str and de.VR != "OW":
-            tags += '{"tag": "%s", "keyword": "%s", "valor":"%s"},' % (str(de.tag), de.keyword, str(de.value))
+        if de.VR != "OB":
+            if de.VR != "OW" and de.VR != 'SQ':
+                # modelo anterior tags += '{"tag": "%s", "keyword": "%s", "valor":"%s"},' % (str(de.tag), de.keyword, str(de.value))
+                valor = str(de.value).replace("[", "", ).replace("]", "", ).replace("'", "", )
+
+                if (len(de.keyword) > 0):
+                    tags += '"%s": "%s",' % (de.keyword, valor)
+                else:
+                    tags += '"%s": "%s",' % (de.name, valor)
         # print ('{"tag": "%s", "keyword": "%s", "valor":"%s"},' % (str(de.tag), de.keyword, str(de.value)))
     # print(tags);
 
-    insereCouch(str(dataset.SOPInstanceUID), '{"tags": [ ' + tags[:-1] + ']}')
+    insereCouch(str(dataset.SOPInstanceUID), '{' + tags[:-1] + "}")
+    #'{"tags": [ ' + tags[:-1] + ']}')
+
+
     return dataset.SOPInstanceUID
